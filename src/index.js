c2d586b3e8af99c4aa40637cc9cf313f2378fec9
Gilson96 <grafael99@gmail.com>
2024-07-19 15:40:09 +0100

    Added HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import App from './App'
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import {store} from './store'
 import { ChakraProvider } from '@chakra-ui/react'
